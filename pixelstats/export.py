64fa465e3a5ddb2f3ce2fe3db9336d89ccc4b14c
Michael Mulhearn <mulhearn@physics.ucdavis.edu>
2018-09-13 22:20:16 -0700

    add hashes to calibrations for offline consistency checks

--- pixelstats/export.py
+++ pixelstats/export.py
@@ -41,10 +41,12 @@ def analysis(args):
     hot       = hots['hot_list']
 
     print "number of hot cells:        ", hot.size
+    print "maximum index:              ", np.max(hot)
     print "shape of lens shading map:  ", lens.shape
     print "down sample:                ", ds
     print "width:                      ", width
     print "height:                     ", height
+    print "max pixel:                  ", width*height
     ly = lens.shape[0]
     lx = lens.shape[1]
     print "lx:                         ", lx
@@ -61,7 +63,29 @@ def analysis(args):
     print "weight 0:                   ", wgt[0]
     print "weight 1:                   ", wgt[1]
 
+    hot_hash = hash(hot.tostring())&0x7fffffff
+    wgt_hash = hash(wgt.tostring())&0x7fffffff
+
+
+    print "hash hot pixels:  ", hot_hash
+    print "hash wgt:         ", wgt_hash
+
+    #
+    # Output for emulation:
+    #
+    down, nx, ny = down_sample(width, height, ds, ds)    
+    denom = 1024
+    hw_wgt = (wgt.astype("f4")*denom).astype("i4")[down]
+    hw_wgt[hot] = 0
+    hw_wgt = hw_wgt.reshape((height,width))
+    np.savez("calib/weight.npz",hot_hash=hot_hash, wgt_hash=wgt_hash, denom=denom,wgt=hw_wgt);    
+
+    #
+    # Output to Java
+    # 
+
     h = np.array([],dtype=">i4")
+    h = np.append(h,wgt_hash)
     h = np.append(h,width)
     h = np.append(h,height)
     h = np.append(h,ds)
@@ -73,13 +97,12 @@ def analysis(args):
         wgt.astype(">f4").tofile(f)
 
     h = np.array([])
+    h = np.append(h,hot_hash)
     h = np.append(h,hot.size)
     h = np.append(h,hot)
 
     with open('calib/hot_pixels.cal', 'w+') as f:
         h.astype(">i4").tofile(f)
-
-
 
     return
     
