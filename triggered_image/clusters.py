1106054def992b7e541dc5654aea0701d6d9924e
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2019-09-04 18:22:23 -0700

    Efficiency improvements

--- triggered_image/clusters.py
+++ triggered_image/clusters.py
@@ -1,108 +1,83 @@
-import numpy
+#!/usr/bin/env python3
+
+import numpy as np
 import argparse
 from sklearn.cluster import DBSCAN, AgglomerativeClustering
 import pathlib
+import os
+from time import time
 
+def cluster(x, y, val, threshold, clusteringOption=1):
 
-def maxValue(place_holder, luminosity):
-    highestLumIndice = numpy.argmax(luminosity[place_holder[0]]);
-    return highestLumIndice;
-
-def indicesOfMaxLuminosity(place_holders, array, locations):
-    
-    return array[place_holders[0]][locations[place_holders[0]]]; 
-
-def brightest(groups,luminosity):
-    place_holder = numpy.arange(len(luminosity));
-    place_holder = place_holder.reshape(len(luminosity),1);
-    
-    indicesForParticularGroups = numpy.apply_along_axis(maxValue,1, place_holder, luminosity);
-    
-    indicesOfMaxLuminosityArray = numpy.apply_along_axis(indicesOfMaxLuminosity, 1, place_holder, groups, indicesForParticularGroups);
-    
-    return indicesOfMaxLuminosityArray;
-
-
-def average(place_holder, xy, groups):
-    return numpy.take(xy, groups[place_holder[0]]).sum() / (groups[place_holder[0]].size);
-
-
-
-
-parser = argparse.ArgumentParser();
-parser.add_argument('-dt', type = float, nargs = 1, help = "set distance threshold", required = True);
-
-parser.add_argument('-f', type = str, nargs = '+', help = "pick npz files that contain data for clustering", required = True);
-
-parser.add_argument('-c', type = int, nargs = 1, help = "pick a clustering algorithm.  1: DBSCAN, 2: Agglomerative Clustering", default = [1]);
-
-parser.add_argument('-out', type = str, nargs = 1, help = "select an output file");
-
-args = parser.parse_args();
-
-threshold = args.dt[0];
-f = args.f;
-clusteringOption = args.c[0];
-
-
-for files in f:
-
-    x = numpy.load(files)['x'];
-    y = numpy.load(files)['y'];
-    xy = numpy.column_stack((x,y));
-    luminosity = numpy.load(files)['luminosities'];
-    
-    
+    # create clusters
+    xy = np.column_stack((x,y)) 
+        
     if clusteringOption == 1:
-        clustering = DBSCAN(eps = threshold, min_samples = 1);
-        clustering.fit(xy); 
+        clustering = DBSCAN(eps = threshold, min_samples = 1) 
     elif clusteringOption == 2:
-        clustering = AgglomerativeClustering(n_clusters = None, compute_full_tree = True, distance_threshold = threshold);
-        clustering.fit(xy);
+        clustering = AgglomerativeClustering(n_clusters = None, compute_full_tree = True, distance_threshold = threshold)
 
-    
-    ordered_indices = numpy.argsort(clustering.labels_);
-    ordered_labels = numpy.sort(clustering.labels_);
-    
+    clustering.fit(xy)
 
-    diff = numpy.diff(ordered_labels);
-    locations_to_split = numpy.argwhere(diff != 0) + 1;
+    ordered_indices = np.argsort(clustering.labels_)
+    ordered_labels = clustering.labels_[ordered_indices]
 
-    groups = numpy.array_split(ordered_indices, locations_to_split.flatten());
+    # split into groups
+    diff = np.diff(ordered_labels)
+    locations_to_split = (np.argwhere(diff != 0) + 1).flatten()
 
-    luminosity_split = numpy.array_split(numpy.take(luminosity,ordered_indices),locations_to_split.flatten());
-
-
-
-    group_location_indices = brightest(groups, luminosity_split);
-    
-    #calculates locations of groups based on the greatest luminosity
-    highest_lum_x = numpy.take(x, group_location_indices.astype('int'));
-    highest_lum_y = numpy.take(y, group_location_indices.astype('int'));
+    groups = np.array_split(ordered_indices, locations_to_split)
             
+    # calculates locations of groups based on the greatest luminosity
+    group_location_indices = [gp[np.argmax(val[gp])] for gp in groups]
+    highest_lum_x = x[group_location_indices]
+    highest_lum_y = y[group_location_indices]
+        
+    # calculates locations of groups based on a weighted average
+    xyval = xy * val.reshape(-1,1)
+    avg_x, avg_y = np.array([xyval[gp].sum(axis=0) / val[gp].sum() for gp in groups]).transpose()
+
+    return highest_lum_x, highest_lum_y, avg_x, avg_y
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('files', type = str, nargs = '+', help = "pick npz files that contain data for clustering")
+    parser.add_argument('--thresh', type = float, help = "set distance threshold", required = True)
+    parser.add_argument('--cluster', type = int, help = "pick a clustering algorithm.  1: DBSCAN, 2: Agglomerative Clustering", default = 1)
+    parser.add_argument('--out', type = str, default='cluster', help = 'sets directory for .npz files to be saved into.')
+
+    args = parser.parse_args()
+
+    threshold = args.thresh
+
+
+    for i,fname in enumerate(args.files):
+        print('{} / {}'.format(i+1, len(args.files)), end='\r')
+
+
+        f = np.load(fname)
+        x = f['x']
+        y = f['y']
+        val = f['val']
+        f.close()
+
+        max_x, max_y, avg_x, avg_y = cluster(x, y, val, args.thresh, args.cluster)
+
+        inpath = pathlib.PurePath(os.path.realpath(fname))
+        outdir = pathlib.Path(inpath.parent.parent.joinpath(args.out))
+        if not outdir.is_dir():
+            outdir.mkdir(parents = True)
+
+        outbase = inpath
+        while outbase.suffix:
+            outbase = pathlib.PurePath(outbase.stem)
+        outbase = outbase.name
     
-    #calculates locations of groups based on a weighted average
-    avg_x = numpy.apply_along_axis(average, 1, numpy.arange(len(groups)).reshape(len(groups),1), x, groups);
-    avg_y = numpy.apply_along_axis(average, 1, numpy.arange(len(groups)).reshape(len(groups),1), y, groups);
-   
-
-    if args.out:
-        
-        if args.c[0] == 1:
-            outFile = args.out[0] + '/' + files.split('.')[0] + " DBSCAN Clusters.npz";
-        elif args.c[0] == 2:
-            outFile = args.out[0] + '/' + files.split('.')[0] + " AgglomerativeClustering Clusters.npz";
-
-        Path = pathlib.Path(args.out[0]);
-        if not Path.is_dir():
-            Path.mkdir(parents = True);
-
-    else:
-        if args.c[0] == 1:
-            outFile = files.split('.')[0] + " DBSCAN Clusters.npz";
-        elif args.c[0] == 2:
-            outFile = files.split('.')[0] + " AgglomerativeClustering Clusters.npz";
-        
-    numpy.savez(outFile, highest_lum_x = highest_lum_x, highest_lum_y = highest_lum_y, avg_x = avg_x, avg_y = avg_y);
+        outfile = str(outdir.joinpath(outbase))
+            
+        np.savez(outfile, max_x=max_x, max_y=max_y, avg_x=avg_x, avg_y=avg_y)
 
 
--- triggered_image/threshold.py
+++ triggered_image/threshold.py
@@ -1,80 +1,81 @@
+#!/usr/bin/env python3
+
 import rawpy
-import numpy
+import numpy as np
 import argparse
 import pathlib
+import gzip
 
 
+def hotcells(array, hotc): 
 
+    x_res = array.shape[1]
+    hotx = hotc % (x_res)
+    hoty = hotc // (x_res)
 
-def hotcells(array, x_res):
-    
-    hotc = numpy.load(hotcellFile);
-    hotc_array = hotc['hot'];
-    x = hotc_array % (x_res);
-    y = hotc_array // (x_res);
+    maskedarray = np.ma.masked_array(array)
+    maskedarray[hoty,hotx] = np.ma.masked
+    return np.ma.filled(maskedarray,0)
 
-    maskedarray = numpy.ma.masked_array(array);
-    maskedarray[y,x] = numpy.ma.masked;
-    return numpy.ma.filled(maskedarray,0);
+def checkPixels(array, threshold):
 
-def checkPixels(array,size):
-        
-    aboveThreshold = numpy.array(numpy.nonzero(array > threshold));
-
-    x = aboveThreshold[1];
-    y = aboveThreshold[0];
-    luminosities = array[array > threshold];
-    data = numpy.column_stack((aboveThreshold[1], aboveThreshold[0], luminosities));
-    numberOfDetections = len(luminosities);
-    
-    if args.out:
-
-        pathName = pathlib.Path(outputDirectory);
-
-        if not pathName.is_dir():
-            pathName.mkdir(parents = True);
-    
+    y, x = np.array(np.nonzero(array > threshold))
+    val = array[y, x]
    
-    numpy.savez(fileName, x = x, y = y, luminosities = luminosities, data = data, numberOfDetections = numberOfDetections);
+    return x, y, val 
 
 
+def loadImage(infile, thresh, hot=None, out=None):
+    if infile.endswith('.gz'):
+        f = gzip.open(infile)
+    else:
+        f = open(infile)
 
-def loadImage(name):
-    image = rawpy.imread(name);
-    array = image.raw_image;
-    size = array.shape
+    image = rawpy.imread(f)
+    array = image.raw_image
     
-    if args.hot:
-        array = hotcells(array, size[1]);
+    if not hotc is None:
+        array = hotcells(array, hot)
     
-    checkPixels(array,size);
+    x, y, val = checkPixels(array, thresh)
+    
+    image.close()
+    f.close()
+    
+    np.savez(out, x = x, y = y, val = val, thresh=thresh)
 
 
-parser = argparse.ArgumentParser();
-parser.add_argument('-f', type = str, nargs = '+', help = "gives location of image");
-parser.add_argument('-t', type = int, nargs = 1, help = 'sets threshold', required = True);
-parser.add_argument('-out', type = str, nargs = 1, help = 'sets directory for .npz files to be saved into');
-parser.add_argument('-hot', type = str, nargs = 1, help = 'gives location of npz file for a hot cell mask');
-args = parser.parse_args();
 
-threshold = args.t[0];
+parser = argparse.ArgumentParser()
+parser.add_argument('files', type = str, nargs = '+', help = "gives location of image")
+parser.add_argument('--thresh', type = int, help = 'sets threshold', required = True)
+parser.add_argument('--out', type = str, default='thresh', help = 'sets directory for .npz files to be saved into')
+parser.add_argument('--hot', type = str, help = 'gives location of npz file for a hot cell mask')
+args = parser.parse_args()
+
 
 if args.hot:
-    hotcellFile = args.hot[0];
-
-if args.out:
-    outputDirectory = args.out[0];
-
-if args.f:
-    for files in args.f:    
-        
-        fileName = files.split('.')[0] + '.npz';
-        
-        if args.out:
-            fileName = outputDirectory + '/' + pathlib.PurePath(fileName).name;
-
-        loadImage(files);
+    hotf = np.load(args.hot)
+    hotc = hotc['hot']
+    hotf.close()
+else:
+    hotc = None
     
 
+for i,f in enumerate(args.files):
+    print('{} / {}'.format(i+1, len(args.files)), end='\r')
 
-       
+    inpath = pathlib.PurePath(os.path.realpath(f))
+    outdir = pathlib.Path(inpath.parent.parent.joinpath(args.out))
+    if not outdir.is_dir():
+        outdir.mkdir(parents = True)
+
+    outbase = inpath
+    while outbase.suffix:
+        outbase = pathlib.PurePath(outbase.stem)
+    outbase = outbase.name
+    
+    outfile = str(outdir.joinpath(outbase))
+ 
+    loadImage(f, args.thresh, hot=hotc, out=outfile)
+    
