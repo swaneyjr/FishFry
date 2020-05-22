1df0ae0d592c0505ac27d494a66edc0bc84157be
swaneyjr <jswaney@uci.edu>
2020-05-22 00:06:06 +0000

    Added logserial.sh

--- hodoscope/remote/install.sh
+++ hodoscope/remote/install.sh
@@ -7,7 +7,7 @@ WORK_DIR=$HOME/hodoscope/
 mkdir -p $WORK_DIR
 mkdir -p $WORK_DIR/logs
 
-cp $DIR/*.py $DIR/*.txt $WORK_DIR
+cp $DIR/*.py $DIR/*.txt $DIR/logserial.sh $WORK_DIR
 cd $WORK_DIR
 
 crontab cron.txt
